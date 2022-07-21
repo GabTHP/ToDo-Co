32cdbd242cf3dcb5e64ef8dd81f215561bccc346
Gabriel Bouakira <gb@ipsip.eu>
2022-07-21 09:14:44 +0200

    create db test and prepare test

--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -2,16 +2,36 @@
 
 namespace Tests\AppBundle\Controller;
 
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
 
+
+    #non connected user tests
     public function testIndexUsers()
     {
         $client = static::createClient();
 
         $client->request('GET', '/users');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateUsers()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/users/create');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+    public function testEditUsers()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/users/1/edit');
 
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
--- tests/AppBundle/Controller/securityControllerTest.php
+++ tests/AppBundle/Controller/securityControllerTest.php
@@ -2,17 +2,68 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase
 {
 
-    public function testIndexUsers()
+
+
+
+    public function testLoginPageUser()
+    {
+        static::bootKernel();
+        $em = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $user = new User();
+        $user->setUsername('gab');
+        $user->setEmail('anonymous@example.org');
+        $plainPassword = 'a';
+        $encoder = static::$kernel->getContainer()->get('security.password_encoder');
+        $encoded = $encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
+        $user->setRoles(['ROLE_USER']);
+
+        $em->persist($user);
+        $em->flush();
+
+        $client = static::createClient();
+
+        $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+    public function testLoginActionSuccess()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/users');
+        $crawler = $client->request('GET', '/login');
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'gab';
+        $form['_password'] = 'a';
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame("Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !", $crawler->filter('h1')->text());
+    }
+
+    public function testLoginActionFail()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'fail';
+        $form['_password'] = 'fail';
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame("Invalid credentials.", $crawler->filter('div.alert.alert-danger')->text());
     }
 }
