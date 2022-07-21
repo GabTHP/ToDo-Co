117bcba7cf297d793380634eec2fb44a94d3675d
Gabriel Bouakira <gb@ipsip.eu>
2022-07-21 14:28:35 +0200

    user controller tests ok

--- app/tests.bootstrap.php
+++ app/tests.bootstrap.php
@@ -2,7 +2,6 @@
 // delete database if exists, then create
 passthru('php bin/console doctrine:database:drop --env=test --force --if-exists');
 passthru('php bin/console doctrine:database:create --env=test');
-passthru('php bin/console doctrine:migrations:migrate --env=test -n');
 passthru('php bin/console doctrine:schema:update --env=test --force');
 
 // run migrations
